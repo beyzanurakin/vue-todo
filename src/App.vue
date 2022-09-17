3d47603a333c6ca7dc931074e247a958b9ee86e5
beyzanurakin <akinbeyzanur@gmail.com>
2022-09-17 21:19:59 +0300

    Finish

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,11 @@ const addTodo = () => {
     done: false,
     createdAt: new Date().getTime(),
   })
+  input_content.value = ''
+  input_category.value = null
+}
+const removeTodo = (todo) => {
+  todos.value = todos.value.filter((t) => t !== todo)
 }
 watch(
   todos,
@@ -57,7 +62,6 @@ onMounted(() => {
           placeholder="e.g. make a video"
           v-model="input_content"
         />
-        {{ input_content }}
         <h4>Pick a category</h4>
         <div class="options">
           <label>
@@ -74,18 +78,38 @@ onMounted(() => {
             <input
               type="radio"
               name="category"
-              value="personel"
+              value="personal"
               v-model="input_category"
             />
             <span class="bubble personal"></span>
             <div>Personal</div>
           </label>
-          {{ input_category }}
         </div>
 
         <input type="submit" value="Add Todo" />
       </form>
     </section>
-    {{ todos_asc }}
+    <section class="todo-list">
+      <h3>TODO LIST</h3>
+      <div class="list">
+        <div
+          v-for="todo in todos_asc"
+          :class="`todo-item ${todo.done && 'done'}`"
+        >
+          <label>
+            <input type="checkbox" v-model="todo.done" />
+            <span :class="`bubble ${todo.category}`"></span>
+          </label>
+
+          <div class="todo-content">
+            <input type="text" v-model="todo.content" />
+          </div>
+
+          <div class="actions">
+            <button class="delete" @click="removeTodo(todo)">Delete</button>
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
